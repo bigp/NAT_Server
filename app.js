1f2fe2e965b86a37372263510c0d00b778147ba9
Pierre Chamberlain <bigp@pierrechamberlain.ca>
2017-04-19 23:34:22 -0300

    port fix.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const globals = require('./common/globals');
 
 trace("HI!");
 
-BIGP.server.listen(3333);
+BIGP.server.listen(process.env.PORT || 3000);
 
 BIGP.app.get('/', (req, res, next) => {
 	res.send("Oh hi!");
